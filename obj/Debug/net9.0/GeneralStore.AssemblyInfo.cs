8163bca5e6f2590ea4b274eec1ec4354f83f9bbd
ArlanTleuberlinQA <lovitopor68@gmail.com>
2025-08-21 19:23:51 +0300

    fx20

--- obj/Debug/net9.0/GeneralStore.AssemblyInfo.cs
+++ obj/Debug/net9.0/GeneralStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GeneralStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8be524211996b261e6995c5daaa0515c2801a036")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+385c61deb1e9e97486e6e6c4e9cb6eee4911152a")]
 [assembly: System.Reflection.AssemblyProductAttribute("GeneralStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GeneralStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

7923e24c1a8d4b23fbe0830987c39b829310cbb8
ArlanTleuberlinQA <lovitopor68@gmail.com>
2025-08-25 16:11:59 +0300

    fx22

--- Configs/AppiumConfig.cs
+++ Configs/AppiumConfig.cs
@@ -37,9 +37,9 @@ public static AppiumOptions BuildAndroidOptions()
     opts.AddAdditionalAppiumOption("newCommandTimeout", 300);
 
     // важливе
-    opts.AddAdditionalAppiumOption("skipServerInstallation", true);
+    opts.AddAdditionalAppiumOption("skipServerInstallation", false);
     opts.AddAdditionalAppiumOption("skipDeviceInitialization", false);
-    opts.AddAdditionalAppiumOption("skipSettingsAppInstallation", true);
+    opts.AddAdditionalAppiumOption("skipSettingsAppInstallation", false);
     opts.AddAdditionalAppiumOption("disableWindowAnimation", true);
 
     return opts;
